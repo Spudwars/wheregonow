d37f9003f41556be5a0628b7c2dc03bf7ba66037
Chris Jesse <chris.jesse@flightdataservices.com>
2013-02-05 22:21:35 +0000

    added sexual preference to UserProfile, lat/long to Place, Vote information and a new VoteVerification to allow multiple users to verify a vote

--- places/models.py
+++ places/models.py
@@ -14,18 +14,37 @@ class UserProfile(models.Model):
  
     SEX_CHOICES = (('male', 'Male'),
                    ('female', 'Female'))
+    
+    PREFERENCE_CHOICES = (('men', 'Men'),
+                          ('women', 'Women'),
+                          ('both', 'Both'))
+                                  
         
     user = models.ForeignKey(User, unique=True)
     #url = models.URLField("Website", blank=True)
     sex = models.CharField(max_length=10, blank=True, choices=SEX_CHOICES)
-
+    preference = models.CharField(max_length=10, blank=True, 
+                                  choices=PREFERENCE_CHOICES)
 
 class Place(models.Model):
     '''
     '''
     name = models.CharField(max_length=128)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
     
     
 class Vote(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name='votes')
     place = models.ForeignKey(Place)
+    ratio = models.IntegerField(default=50, 
+                    help_text='Only Men 1, 25, 50, 75, 99  Only Women')
+    hotness = models.IntegerField(default=0)
+    
+    
+class VoteVerification(models.Model):
+    user = models.ForeignKey(User, related_name='verifications')
+    verified = models.BooleanField()
+    
+    
+#TODO: Add comments to Vote (and verifications?)
