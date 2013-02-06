c438d6cfbd69a4cce8d2dee7d1a769cf22fbf030
Chris Jesse <chris.jesse@flightdataservices.com>
2013-02-06 09:49:39 +0000

    social_auth urls added

--- places/urls.py
+++ places/urls.py
@@ -14,18 +14,25 @@ urlpatterns = patterns('',
     url(r'^profiles/', include('profiles.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
     
-    url(r'', include('social_auth.urls')),  #All django-social-auth URLs names have socialauth_ prefix.
-
-
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     
 
-    url(r'^admin/', include(admin.site.urls)),
 
-    # WARNING: Not production-friendly!
-    url(r'^sentry/', include('sentry.web.urls')),
+    url(r'^done/$', done, name='done'),
+    url(r'^error/$', error, name='error'),
+    url(r'^logout/$', logout, name='logout'),
+    #url(r'^form/$', form, name='form'),
+    #url(r'^form2/$', form2, name='form2'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^fb/', facebook_view, name='fb_app'),
+    url(r'^vk/', vkontakte_view, name='vk_app'),
+    url(r'^ok/$', ok_app, name='ok_app'),
+    url(r'^ok/info/$', ok_app_info, name='ok_app_info'),
+    url(r'^close_login_popup/$', close_login_popup, name='login_popup_close'),
+    
+    
+    url(r'', include('social_auth.urls')),  #All django-social-auth URLs names have socialauth_ prefix.
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^sentry/', include('sentry.web.urls')),  # WARNING: Not production-friendly!
 )
 
 urlpatterns += patterns('django.views.generic.simple',
